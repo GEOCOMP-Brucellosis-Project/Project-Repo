f082c3ba8482c8f734a0c6159e8e0baef58caa7a
tannerjohnson5 <tannerjohnson56@gmail.com>
2020-04-03 13:14:36 -0500

    Update EE_env_params.py

--- EE_env_params.py
+++ EE_env_params.py
@@ -3,16 +3,8 @@
 Bovine Brucellosis Project Code
 
 To-do:
-Write code to read-in animal and human data
-Write code to calculate a brucellosis data metric that we want to use (can be just a placeholder for now! maybe infected/total bovine?)
-Create dataframe with brucellosis data metric AND temperature+precipitation data
-Create function to plot the data with Matplotlib!
-
-
-Back-burner:
-Create function to get mean monthly temperature
-Create function to get total monthly precipitation
-
+NDVI
+Elevation/Terrain
 
 
 #ideas!
@@ -30,7 +22,7 @@ ee.Initialize()
 ######### Functions #############
 def imageParams(image, shapefile):
     """
-    Takes an image and a shapefile, returns a dataframe with the parameters for each county for each month. 
+    Takes an image and a shapefile, returns a dataframe with the mean band values summarized by shapefile feature. 
     """
     #Gets the average of the band values in each feature in the iran asset. See: https://developers.google.com/earth-engine/reducers_reduce_region
     envFC=image.reduceRegions(shapefile, ee.Reducer.mean())
@@ -71,71 +63,8 @@ def getYearlyParams(collection, shapefile, startYear, endYear):
 ############# Main ######################
 
 moWeather = ee.ImageCollection("ECMWF/ERA5/MONTHLY").select("mean_2m_air_temperature","total_precipitation") #Loads the ERA5 monthly collection
-iran = ee.FeatureCollection("users/tannerjohnson56/iran_admin").select("ADM2_EN") #Loads the iran boundaries asset with only county name field kept
+iran = ee.FeatureCollection("https://code.earthengine.google.com/?asset=users/tannerjohnson56/iran_admin").select("ADM2_EN") #users/tannerjohnson56/iran_admin").select("ADM2_EN") #Loads the iran boundaries asset with only county name field kept
 
 
-yearlyParams=getYearlyParams(moWeather, iran, 2008, 2018)
-yearlyParams.to_csv(r'yearlyParams.csv',index = False)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################OLD CODE
-
-
-#old fashioned way
-# image1=moWeather.filter(ee.Filter.calendarRange(2010, 2010, 'year'))\
-#     .filter(ee.Filter.calendarRange(1, 1, 'month')).first()
-
-# image2=moWeather.filter(ee.Filter.calendarRange(2010, 2010, 'year'))\
-#     .filter(ee.Filter.calendarRange(2, 2, 'month')).first()
-    
-
-# db0=pandas.DataFrame(columns=['ADM2_EN'])
-# db1=imageParams(image1, iran)
-# db2=imageParams(image2, iran)
-
-# db3=db0.merge(db1, how='outer', on='ADM2_EN', suffixes=('', '_201001'))
-# db3=db1.merge(db2, how='outer', on='ADM2_EN', suffixes=('', '_201002'))
-
-
-
-
-
-
-"""withEnv=day.reduceRegions(iran, ee.Reducer.mean()) 
-
-#.getInfo() is used in ee to actually store the values locally since values are normally stored, manipulated, and mapped in the cloud. 
-features = withEnv.getInfo()['features'] 
-
-#Now, features is a messy list of dictionaries. We need to extract the "properties" from each feature. 
-cols=[] #blank variable to store properties
-for feature in features:
-    cols.append(feature['properties']) #adds the properties list to cols
-
-envParams=pandas.DataFrame(cols) #creates a new pandas dataframe with the data in cols
-"""
-
-
-#envParams.to_csv(r'envParams.csv',index = False)
-
-
-
-# #one issue so far: unmatched county names
-# s1=set(animal_data.County)
-# s2=set(envParams.ADM2_EN)
-# matched = s1.intersection(s2) 
-# unmatched = s1.symmetric_difference(s2)
-# len(matched)
-# len(unmatched)
+yearlyParams=getYearlyParams(moWeather, iran, 2008, 2008)
+#yearlyParams.to_csv(r'yearlyParams.csv',index = False)
