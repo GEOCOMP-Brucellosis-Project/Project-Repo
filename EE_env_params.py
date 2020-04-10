a4158e955333087d108c5954f6060ff56e4f7dee
tannerjohnson5 <tannerjohnson56@gmail.com>
2020-04-10 10:04:52 -0500

    Added code for NDVI and elevation
    Plus optional for taking the mean of a month of data rather than the first image from the month.

--- EE_env_params.py
+++ EE_env_params.py
@@ -13,7 +13,6 @@ Create buffer around each animal data point for more accurate env. cond
 
 
 ###Imports
-import calendar
 import ee
 import pandas
 ee.Initialize()
@@ -25,7 +24,7 @@ def imageParams(image, shapefile):
     Takes an image and a shapefile, returns a dataframe with the mean band values summarized by shapefile feature. 
     """
     #Gets the average of the band values in each feature in the iran asset. See: https://developers.google.com/earth-engine/reducers_reduce_region
-    envFC=image.reduceRegions(shapefile, ee.Reducer.mean())
+    envFC=image.reduceRegions(shapefile, ee.Reducer.mean(), image.projection().nominalScale(), image.projection())
     envDict=envFC.getInfo()['features']
 
     #Now, features is a messy list of dictionaries. We need to extract the "properties" from each feature. 
@@ -37,7 +36,7 @@ def imageParams(image, shapefile):
     
 
 
-def getYearlyParams(collection, shapefile, startYear, endYear):
+def getYearlyParams(collection, shapefile, startYear, endYear, first=True):
     """
     Gets the selected yearly parameters as a dataframe for an image collection.
     """
@@ -45,10 +44,14 @@ def getYearlyParams(collection, shapefile, startYear, endYear):
     for year in range(startYear, endYear+1):
         print(year)
         for month in range(1, 13):
-            print(month)
-            image=collection.filter(ee.Filter.calendarRange(year,year,'year'))\
-                .filter(ee.Filter.calendarRange(month, month,'month'))\
+            print(month, end=' ')
+            if(first):
+                image=collection.filter(ee.Filter.calendarRange(year,year,'year')).filter(ee.Filter.calendarRange(month, month,'month'))\
                 .first()
+            else:
+                image=collection.filter(ee.Filter.calendarRange(year,year,'year')).filter(ee.Filter.calendarRange(month, month,'month'))\
+                .mean()
+                
             
             yearlyParams=yearlyParams.merge(imageParams(image, shapefile),\
                                             how='outer',\
@@ -61,10 +64,17 @@ def getYearlyParams(collection, shapefile, startYear, endYear):
 
 
 ############# Main ######################
+iran = ee.FeatureCollection("users/tannerjohnson56/iran_admin").select("ADM2_EN")  #Loads the iran boundaries asset with only county name field kept
 
+dem=ee.Image("JAXA/ALOS/AW3D30/V2_2").select('AVE_DSM')
+ndvi=ee.ImageCollection("NOAA/CDR/AVHRR/NDVI/V5").select('NDVI')
+ndvi2=ee.ImageCollection("MODIS/006/MOD13A2").select('NDVI')
 moWeather = ee.ImageCollection("ECMWF/ERA5/MONTHLY").select("mean_2m_air_temperature","total_precipitation") #Loads the ERA5 monthly collection
-iran = ee.FeatureCollection("https://code.earthengine.google.com/?asset=users/tannerjohnson56/iran_admin").select("ADM2_EN") #users/tannerjohnson56/iran_admin").select("ADM2_EN") #Loads the iran boundaries asset with only county name field kept
 
 
-yearlyParams=getYearlyParams(moWeather, iran, 2008, 2008)
-#yearlyParams.to_csv(r'yearlyParams.csv',index = False)
+weatherParams=getYearlyParams(moWeather, iran, 2008, 2018)
+ndviParams=getYearlyParams(ndvi, iran, 2008, 2018, False)
+elevParams=imageParams(dem, iran)
+
+allParams=weatherParams.merge(ndviParams, on='ADM2_EN').merge(elevParams, on='ADM2_EN')
+allParams.to_csv(r'allParams.csv', index=False)
